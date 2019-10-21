47a2bfb981952d7f27defdb960f528edff76a309
IzabelaGA <izabela.sosnowska23@gmail.com>
2019-10-22 01:29:04 +0200

    Add type writing eff in JS

---
+++ main.js
@@ -0,0 +1,66 @@
+const TypeWritter = function (txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+
+}
+
+// Type Method
+TypeWritter.prototype.type = function () {
+    // Current index of word
+    const current = this.wordIndex % this.words.length;
+    // Get ful text of current word
+    const fullTxt = this.words[current];
+
+    //    check if deleting
+    if (this.isDeleting) {
+        // Remove char
+        this.txt = fullTxt.substring(0, this.txt.length - 1)
+    } else {
+        // Add char
+        this.txt = fullTxt.substring(0, this.txt.length + 1)
+    }
+
+    // Insert txt into element
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    // Type Speed
+    let typeSpeed = 300;
+
+    if (this.isDeleting) {
+        typeSpeed /= 2;
+    }
+
+    // If word is Complete
+    if (!this.isDeleting && this.txt === fullTxt) {
+        // Make pause at end
+        typeSpeed = this.wait;
+        // Set delete to true
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        // Move to next word
+        this.wordIndex++;
+        // Pause before start typing
+        typeSpeed = 500;
+    }
+
+    setTimeout(() => this.type(), typeSpeed);
+}
+
+// Init On DOM Load
+document.addEventListener('DOMContentLoaded', init);
+
+// Init App
+function init() {
+    const txtElement = document.querySelector('.txt-type');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+
+    // Init Typewriter
+    new TypeWritter(txtElement, words, wait);
+}
